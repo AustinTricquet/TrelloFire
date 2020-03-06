9f105436b1927406f4bdec412fa8082a7b910a86
AustinTricquet <austintricquet@gmail.com>
2020-03-06 15:52:16 -0500

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB_I-5kO04rurLZxj6Xtcx52girO4lA5jk",
+    authDomain: "trellofire-bfe1e.firebaseapp.com",
+    databaseURL: "https://trellofire-bfe1e.firebaseio.com",
+    projectId: "trellofire-bfe1e",
+    storageBucket: "trellofire-bfe1e.appspot.com",
+    messagingSenderId: "661354944335",
+    appId: "1:661354944335:web:5019843c69b98cfc8cd73a",
+    measurementId: "G-KFMBP2B96V"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB_I-5kO04rurLZxj6Xtcx52girO4lA5jk",
+    authDomain: "trellofire-bfe1e.firebaseapp.com",
+    databaseURL: "https://trellofire-bfe1e.firebaseio.com",
+    projectId: "trellofire-bfe1e",
+    storageBucket: "trellofire-bfe1e.appspot.com",
+    messagingSenderId: "661354944335",
+    appId: "1:661354944335:web:5019843c69b98cfc8cd73a",
+    measurementId: "G-KFMBP2B96V"
+  }
 };
 
 /*
